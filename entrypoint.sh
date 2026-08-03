f735a48a05a9323aa897af5941fecb5339a2d604
frazon11 <fkryszon@gmail.com>
2026-08-03 09:07:41 +0200

    Release v0.6.4.3 move support link below unlock

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-mkdir -p /var/www/data /var/www/backups
-chown -R www-data:www-data /var/www/data /var/www/backups
-
-su -s /bin/sh www-data -c 'php /var/www/html/alert_worker.php' &
-
-exec "$@"

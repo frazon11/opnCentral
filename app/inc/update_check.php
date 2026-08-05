dbe993f159b84445c92c933d28352bdfe130a3c8
FrazoN11 <fkryszon@gmail.com>
2026-08-05 11:56:43 +0200

    Set internal version to v0.6.11.10

--- app/inc/update_check.php
+++ app/inc/update_check.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const OPNCENTRAL_VERSION = '0.6.11.9';
+const OPNCENTRAL_VERSION = '0.6.11.10';
 const OPNCENTRAL_GITHUB_REPOSITORY = 'frazon11/opnCentral';
 const OPNCENTRAL_UPDATE_INTERVAL = 86400;
 
